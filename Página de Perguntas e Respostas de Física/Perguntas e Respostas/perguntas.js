a1fa4e4eceda26c252dc07f1da038a8639bedd78
rikkaah <gmangle98@gmail.com>
2024-10-23 17:54:34 -0300

    Add files via upload

---
+++ Página de Perguntas e Respostas de Física/Perguntas e Respostas/perguntas.js
@@ -0,0 +1,37 @@
+criaCartao(
+    'Física',
+    'Como fica 11 metros convertido em centimetros?',
+    '1 metro = 100 centimetros |  11 metros = 11 x 100 = 1.100 centimetros, logo, 11 metros = 1.100cm'
+
+)
+
+criaCartao(
+    'Física',
+    'Qual é o nome apropriado para o calor cedido pela esfera de metal ao gelo?',
+    'Esse calor cedido pela esfera é chamado de calor sensível, pois ele provoca uma variação de temperatura na esfera, fazendo com que ela esfrie.'
+)
+
+criaCartao(
+    'Física',
+    'Qual é o nome para o calor recebido pelo gelo?',
+    'o calor recebido pelo gelo é utilizado para a mudança de estado físico, ou seja, para derreter o gelo. Portanto, é calor latente de fusão.'
+)
+
+criaCartao(
+    'Física',
+    'O que é o triplo da água?',
+    'corresponde a um ponto, no diagrama de fases, em que os estados sólido, líquido e gasoso coexistem em equilíbrio termodinâmico. No diagrama de fases, o ponto triplo é localizado onde as curvas de sublimação, fusão e vaporização encontram-se.'
+)
+
+criaCartao(
+    'Física',
+    'Analise a afirmativa a seguir e responda se ela é verdadeira ou falsa: "Enquanto a água do exercício anterior fervia, sua temperatura aumentava cada vez mais',
+    'Resposta: FALSO. A afirmativa é falsa, porque durante a mudança de estado físico a água na panela, sob pressão constante, não varia sua temperatura.'
+)
+
+criaCartao(
+    'Física',
+    'Em um dia de verão, você viajou para o litoral com seus amigos e decidiu ir à praia. Havia esquecido o chinelo e teria de entrar na praia descalço. Ao pisar na areia, sentia queimar a sola do pé e corria para a água, tentando aliviar a sensação incômoda. Por que a areia próxima à orla está mais quente do  que a areia mais perto do mar?',
+    'A areia próxima à orla está mais quente, porque absorve calor mais rapidamente do que a água. Já a areia próxima ao mar, estando misturada com a água, cede calor para ela, diminuindo assim a temperatura da areia'
+
+)
